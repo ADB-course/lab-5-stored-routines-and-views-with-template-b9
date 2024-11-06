27d8c485fb7f401bf269a9188f374d7704505103
Maxwell619 <maxwell.gatua@strathmore.edu>
2024-11-06 21:55:40 +0300

    proc_lab 5

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,5 +1,23 @@
 -- (i) A Procedure called PROC_LAB5
+DELIMITER $$
 
+CREATE PROCEDURE PROC_LAB5 (IN target_month VARCHAR(7))
+BEGIN
+    SELECT 
+        e.EmployeeID,
+        e.EmployeeName,
+        SUM(s.SaleAmount) AS TotalSales
+    FROM
+        Employees e
+    JOIN Sales s ON e.EmployeeID = s.EmployeeID
+    WHERE DATE_FORMAT(s.SaleDate, '%Y-%m') = target_month
+    GROUP BY e.EmployeeID, e.EmployeeName
+    ORDER BY TotalSales DESC;
+END$$
+
+DELIMITER ;
+
+SHOW PROCEDURE STATUS
 
 -- (ii) A Function called FUNC_LAB5
 
